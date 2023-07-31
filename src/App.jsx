9f378a8461fadba4e59eff0c1b044d835b23ccfb
Linda Andersson <linda.29.andersson@gmail.com>
2023-07-31 11:40:00 +0200

    Changed the color scale

--- src/App.jsx
+++ src/App.jsx
@@ -63,9 +63,7 @@ function App() {
     svg.append("g").attr("id", "y-axis").call(yAxis);
 
     // Define a color scale
-    const colorScale = d3
-      .scaleSequential(d3.interpolateRdYlBu)
-      .domain(d3.extent(data.monthlyVariance, (d) => d.variance));
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
     // Create the heat map cells
     svg
